fcc652ed06793dd70f2bd011e23e2f5ca6842a88
Richard Kroesen <kroesen.ri@gmail.com>
2023-03-06 15:26:27 +0100

    -enh refactored sensor compression.

--- modules/hal_sensors/sensor_compression/sensor_compression.hpp
+++ modules/hal_sensors/sensor_compression/sensor_compression.hpp
@@ -2,25 +2,35 @@
 #define SENSOR_COMPRESSION_HPP
 
 #include "sensor_base.hpp"
-#include "drivers/VL6180x.hpp"
+#include "VL6180X_REGISTERS.hpp"
+#include <i2c_helper.hpp>
+
+#define SENSOR_ADDR (uint8_t)0x29
 
 class CompressionSensor : public UniversalSensor {
  public:
-  explicit CompressionSensor(i2c_peripheral_t i2c_peripheral)
-      : UniversalSensor(i2c_peripheral) {
-    i2c_handle_ = new I2CDriver(i2c_peripheral, ki2cSpeed_400KHz, kSensorI2CAddress_);
-    Tof_ = new RobotPatient_VL6180x(i2c_handle_);
+  explicit CompressionSensor(i2c_peripheral_t i2c_peripheral) : UniversalSensor(i2c_peripheral) { 
+    i2c_handle_ = new I2CDriver(i2c_peripheral, ki2cSpeed_400KHz, kSensorI2CAddress_); 
   }
+
   void Initialize() override;
   SensorData GetSensorData() override;
   void Uninitialize() override;
   ~CompressionSensor() {
     Uninitialize();
   }
+
  private:
-  const uint8_t kSensorI2CAddress_ = 0x29;
+  uint8_t kSensorI2CAddress_ = SENSOR_ADDR; // ! Was CONSTANT, for now rm const. !
   SensorData sensor_data_{};
   I2CDriver *i2c_handle_;
-  RobotPatient_VL6180x *Tof_;
+
+// Low level driver functions:
+  uint8_t initVL6180x(void);
+  void VL6180xDefautSettings(void);
+  uint8_t getDistance(void);
+  float getAmbientLight(vl6180x_als_gain VL6180X_ALS_GAIN);
+  void getIdentification(struct VL6180xIdentification *dest);
+  uint8_t changeAddress(uint8_t old_address, uint8_t new_address);
 };
 #endif  // SENSOR_COMPRESSION_HPP
