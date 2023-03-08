98033a5698fbb72fa11a7199a561561726d19a28
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-03-08 20:15:17 +0100

    -enh remove duplicate files

--- modules/hal_sensors/sensor_base/sensor_helper.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SENSOR_HELPER_HPP
-#define SENSOR_HELPER_HPP
-
-#include <sensor_base.hpp>
-#include <sensor_compression.hpp>
-#include <sensor_differentialpressure.hpp>
-#include <sensor_fingerposition.hpp>
-
-#endif
