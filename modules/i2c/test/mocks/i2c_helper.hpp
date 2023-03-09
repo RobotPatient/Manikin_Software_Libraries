04267e7466fcc8459d461b17f0af51dfcf77568d
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-03-09 14:47:01 +0100

    -enh reformated code in i2c_helper.hpp(mock)

--- modules/i2c/test/mocks/i2c_helper.hpp
+++ modules/i2c/test/mocks/i2c_helper.hpp
@@ -8,20 +8,20 @@
 #define i2c_peripheral_t I2CPeripheralMock*
 
 typedef enum {
- ki2cSpeed_100KHz= 100000,  ki2cSpeed_400KHz = 400000,
+  ki2cSpeed_100KHz = 100000, ki2cSpeed_400KHz = 400000,
 } i2c_speed_t;
 
 class I2CDriver {
-    public:
-    MOCK_METHOD(void, init, ());
-    MOCK_METHOD(void, WriteReg, (uint16_t reg, uint8_t data));
-    MOCK_METHOD(void, WriteReg16, (uint16_t reg, uint16_t data));
-    MOCK_METHOD(uint8_t, ReadReg, (uint16_t reg));
-    MOCK_METHOD(uint16_t, ReadReg16, (uint16_t reg));
-    MOCK_METHOD(void, ReadBytes, (uint8_t* buffer, uint8_t num_of_bytes));
-    MOCK_METHOD(void, SendBytes, (uint8_t* buffer, uint8_t num_of_bytes));
-    MOCK_METHOD(void, ChangeAddress, (uint8_t new_i2c_address));
-    MOCK_METHOD(void, constructor_called, (i2c_peripheral_t i2c_peripheral, i2c_speed_t speed, uint8_t i2c_addr));
+ public:
+  MOCK_METHOD(void, init, ());
+  MOCK_METHOD(void, WriteReg, (uint16_t reg, uint8_t data));
+  MOCK_METHOD(void, WriteReg16, (uint16_t reg, uint16_t data));
+  MOCK_METHOD(uint8_t, ReadReg, (uint16_t reg));
+  MOCK_METHOD(uint16_t, ReadReg16, (uint16_t reg));
+  MOCK_METHOD(void, ReadBytes, (uint8_t * buffer, uint8_t num_of_bytes));
+  MOCK_METHOD(void, SendBytes, (uint8_t * buffer, uint8_t num_of_bytes));
+  MOCK_METHOD(void, ChangeAddress, (uint8_t new_i2c_address));
+  MOCK_METHOD(void, constructor_called, (i2c_peripheral_t i2c_peripheral, i2c_speed_t speed, uint8_t i2c_addr));
 };
 
 #endif
