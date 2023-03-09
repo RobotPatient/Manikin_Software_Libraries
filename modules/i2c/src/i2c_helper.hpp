9029e5b2b2a2a50839fdd4cd5147eeb078565cac
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-03-09 14:46:29 +0100

    -enh reformated code in i2c_helper.hpp

--- modules/i2c/src/i2c_helper.hpp
+++ modules/i2c/src/i2c_helper.hpp
@@ -10,35 +10,35 @@
 #define i2c_peripheral_t I2CPeripheralMock*
 #endif
 
-typedef enum{
-ki2cSpeed_100KHz= 100000,  ki2cSpeed_400KHz = 400000,
-}i2c_speed_t;
+typedef enum {
+  ki2cSpeed_100KHz = 100000, ki2cSpeed_400KHz = 400000,
+} i2c_speed_t;
 
-class I2CDriver{
-    public:
-    I2CDriver(i2c_peripheral_t i2c_peripheral, i2c_speed_t speed, uint8_t i2c_addr){
-        this->_i2c_peripheral = i2c_peripheral;
-        this->_i2c_addr = i2c_addr;
-        this->_speed=speed;                                 
-    }
-    
-    I2CDriver(i2c_peripheral_t i2c_peripheral, i2c_speed_t speed){
-        this->_i2c_peripheral = i2c_peripheral;
-        this->_speed=speed;                                 
-    }
-    void init();
-    void WriteReg(uint16_t reg, uint8_t data);
-    void WriteReg16(uint16_t reg, uint16_t data);
-    uint8_t ReadReg (uint16_t reg);
-    uint16_t ReadReg16(uint16_t reg);
-	
-	void ReadBytes(uint8_t* buffer, uint8_t num_of_bytes);
-	void SendBytes(uint8_t* buffer, uint8_t num_of_bytes);
-	void ChangeAddress(uint8_t new_i2c_address);
-    private:
-    uint8_t _i2c_addr;
-    i2c_peripheral_t _i2c_peripheral;
-    i2c_speed_t _speed;
+class I2CDriver {
+ public:
+  I2CDriver(i2c_peripheral_t i2c_peripheral, i2c_speed_t speed, uint8_t i2c_addr) {
+    this->_i2c_peripheral = i2c_peripheral;
+    this->_i2c_addr = i2c_addr;
+    this->_speed = speed;
+  }
+
+  I2CDriver(i2c_peripheral_t i2c_peripheral, i2c_speed_t speed) {
+    this->_i2c_peripheral = i2c_peripheral;
+    this->_speed = speed;
+  }
+  void init();
+  void WriteReg(uint16_t reg, uint8_t data);
+  void WriteReg16(uint16_t reg, uint16_t data);
+  uint8_t ReadReg(uint16_t reg);
+  uint16_t ReadReg16(uint16_t reg);
+
+  void ReadBytes(uint8_t *buffer, uint8_t num_of_bytes);
+  void SendBytes(uint8_t *buffer, uint8_t num_of_bytes);
+  void ChangeAddress(uint8_t new_i2c_address);
+ private:
+  uint8_t _i2c_addr;
+  i2c_peripheral_t _i2c_peripheral;
+  i2c_speed_t _speed;
 };
 
 #endif
