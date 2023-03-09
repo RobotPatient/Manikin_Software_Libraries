4df09a1fe58cf969f8d9ea24968f0402825410e8
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-03-09 17:17:03 +0100

    -enh auto format i2c module code

--- modules/i2c/src/i2c_helper.cpp
+++ modules/i2c/src/i2c_helper.cpp
@@ -1,34 +1,34 @@
 #include "i2c_helper.hpp"
 
-void I2CDriver::init(){
-    _i2c_peripheral->begin();
+void I2CDriver::init() {
+  _i2c_peripheral->begin();
 }
 
-void I2CDriver::ChangeAddress(uint8_t new_i2c_address){
+void I2CDriver::ChangeAddress(uint8_t new_i2c_address) {
   _i2c_addr = new_i2c_address;
 }
 
-void I2CDriver::WriteReg(uint16_t reg, uint8_t data){
-        _i2c_peripheral->beginTransmission(_i2c_addr);
-    _i2c_peripheral->write((reg >> 8) & 0xFF); // MSB of register address
-    _i2c_peripheral->write(reg & 0xFF);        // LSB of register address
-    _i2c_peripheral->write(data);                       // Data/setting to be sent to device.
-        _i2c_peripheral->endTransmission();
+void I2CDriver::WriteReg(uint16_t reg, uint8_t data) {
+  _i2c_peripheral->beginTransmission(_i2c_addr);
+  _i2c_peripheral->write((reg >> 8) & 0xFF); // MSB of register address
+  _i2c_peripheral->write(reg & 0xFF);        // LSB of register address
+  _i2c_peripheral->write(data);                       // Data/setting to be sent to device.
+  _i2c_peripheral->endTransmission();
 }
 
-void I2CDriver::WriteReg16(uint16_t reg, uint16_t data){
-    _i2c_peripheral->beginTransmission(_i2c_addr);
-    _i2c_peripheral->write((reg >> 8) & 0xFF); // MSB of register address
-    _i2c_peripheral->write(reg & 0xFF);        // LSB of register address
-    uint8_t temp;
-    temp = (data >> 8) & 0xff;
-    _i2c_peripheral->write(temp); // Data/setting to be sent to device
-    temp = data & 0xff;
-    _i2c_peripheral->write(temp);       // Data/setting to be sent to device         // Data/setting to be sent to device.
-    _i2c_peripheral->endTransmission();
+void I2CDriver::WriteReg16(uint16_t reg, uint16_t data) {
+  _i2c_peripheral->beginTransmission(_i2c_addr);
+  _i2c_peripheral->write((reg >> 8) & 0xFF); // MSB of register address
+  _i2c_peripheral->write(reg & 0xFF);        // LSB of register address
+  uint8_t temp;
+  temp = (data >> 8) & 0xff;
+  _i2c_peripheral->write(temp); // Data/setting to be sent to device
+  temp = data & 0xff;
+  _i2c_peripheral->write(temp);       // Data/setting to be sent to device         // Data/setting to be sent to device.
+  _i2c_peripheral->endTransmission();
 }
 
-uint8_t I2CDriver::ReadReg (uint16_t reg){
+uint8_t I2CDriver::ReadReg(uint16_t reg) {
   uint8_t data;
 
   _i2c_peripheral->beginTransmission(_i2c_addr);    // Address set on class instantiation
@@ -40,7 +40,7 @@ uint8_t I2CDriver::ReadReg (uint16_t reg){
   return data;
 }
 
-uint16_t I2CDriver::ReadReg16(uint16_t reg){
+uint16_t I2CDriver::ReadReg16(uint16_t reg) {
   uint8_t data_low;
   uint8_t data_high;
   uint16_t data;
@@ -55,12 +55,12 @@ uint16_t I2CDriver::ReadReg16(uint16_t reg){
   return data;
 }
 
-void I2CDriver::ReadBytes(uint8_t* buffer, uint8_t num_of_bytes){
+void I2CDriver::ReadBytes(uint8_t *buffer, uint8_t num_of_bytes) {
   _i2c_peripheral->requestFrom(_i2c_addr, num_of_bytes, true);
   _i2c_peripheral->readBytes(buffer, num_of_bytes);
 }
 
-void I2CDriver::SendBytes(uint8_t* buffer, uint8_t num_of_bytes){
+void I2CDriver::SendBytes(uint8_t *buffer, uint8_t num_of_bytes) {
   _i2c_peripheral->beginTransmission(_i2c_addr);
   _i2c_peripheral->write(buffer, num_of_bytes);
   _i2c_peripheral->endTransmission(true);
