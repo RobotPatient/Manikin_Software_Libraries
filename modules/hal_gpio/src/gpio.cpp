88cecfcfeda77eb9c39276a58113e0c1ddc5d1dc
Thomas <TCJ.vandenOever@student.han.nl>
2023-03-30 10:21:08 +0200

    -enh second rev of hal gpio lib

--- modules/hal_gpio/src/gpio.cpp
+++ modules/hal_gpio/src/gpio.cpp
@@ -9,45 +9,92 @@
 #include <samd51/include/samd51g19a.h>
 #endif
 
-namespace hal::gpio{
+namespace hal::gpio
+{
 
-
-void SetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num, uint8_t level){
-if(level){
-    PORT->Group[port_num].OUTSET.reg |= 1 << pin_num;
-}
-else{
-    PORT->Group[port_num].OUTCLR.reg |= 1 << pin_num;
-}
-}
-
-void ToggleGPIOPin(GPIOPort port_num, uint8_t pin_num){
-    PORT->Group[port_num].OUTTGL.reg |= 1 << pin_num;
-}
-
-void SetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num, uint8_t direction){
-    if(direction){
-        PORT->Group[port_num].DIRSET.reg |= 1 << pin_num;
+    void SetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num, uint8_t level)
+    {
+        if (level)
+        {
+            PORT->Group[port_num].OUTSET.reg |= 1 << pin_num;
+        }
+        else
+        {
+            PORT->Group[port_num].OUTCLR.reg |= 1 << pin_num;
+        }
     }
-    else{
-        PORT->Group[port_num].DIRCLR.reg |= 1 << pin_num;
+
+    void ToggleGPIOPin(GPIOPort port_num, uint8_t pin_num)
+    {
+        PORT->Group[port_num].OUTTGL.reg |= 1 << pin_num;
     }
-}
 
-void SetGPIOPinDriveStrength(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength driver_strength){
-    PORT->Group[port_num].PINCFG[pin_num].bit.DRVSTR = driver_strength ==  kGPIOStrongDriveStrength ? 1 : 0;
-}
-
-void SetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num, GPIOPinFunction pin_function){
-
-}
-
-void SetGPIOPinSamplingMode(GPIOPort port_num, uint8_t pin_num, GPIOSamplingMode sampling_mode){
-    if(sampling_mode == kGPIOSampleContinuously){
-        PORT->Group[port_num].CTRL.reg |= 1 << pin_num;
+    void SetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num, uint8_t direction)
+    {
+        if (direction)
+        {
+            PORT->Group[port_num].DIRSET.reg |= 1 << pin_num;
+        }
+        else
+        {
+            PORT->Group[port_num].DIRCLR.reg |= 1 << pin_num;
+        }
     }
-    else{
-        PORT->Group[port_num].CTRL.reg &= ~(1 << pin_num);
+
+    void SetGPIOPinDriveStrength(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength driver_strength)
+    {
+        PORT->Group[port_num].PINCFG[pin_num].bit.DRVSTR = driver_strength == kGPIOStrongDriveStrength ? 1 : 0;
     }
-}
+
+    void SetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num, GPIOPinFunction pin_function)
+    {
+        PORT->Group[port_num].PINCFG[pin_num].bit.PMUXEN = 0x01;
+        if (pin_num % 2 == 0)
+        {
+            PORT->Group[port_num].PMUX[pin_num].bit.PMUXE |= pin_function;
+        }
+        else
+        {
+            PORT->Group[port_num].PMUX[pin_num].bit.PMUXO |= pin_function;
+        }
+    }
+
+    void SetGPIOPinSamplingMode(GPIOPort port_num, uint8_t pin_num, GPIOSamplingMode sampling_mode)
+    {
+        if (sampling_mode == kGPIOSampleContinuously)
+        {
+            PORT->Group[port_num].CTRL.reg |= 1 << pin_num;
+        }
+        else
+        {
+            PORT->Group[port_num].CTRL.reg &= ~(1 << pin_num);
+        }
+    }
+
+    uint8_t GetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num)
+    {
+        return (PORT->Group[port_num].OUT.reg & 1 << pin_num) | (1 << pin_num);
+    }
+
+    uint8_t GetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num)
+    {
+        return (PORT->Group[port_num].DIR.reg & 1 << pin_num) | (1 << pin_num);
+    }
+
+    uint8_t GetGPIODriveStrength(GPIOPort port_num, uint8_t pin_num)
+    {
+        return (PORT->Group[port_num].PINCFG[pin_num].bit.DRVSTR);
+    }
+
+    uint8_t GetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num)
+    {
+        if (pin_num % 2 == 0)
+        {
+            return PORT->Group[port_num].PMUX[pin_num].bit.PMUXE;
+        }
+        else
+        {
+            return PORT->Group[port_num].PMUX[pin_num].bit.PMUXO;
+        }
+    }
 }
--- modules/hal_gpio/src/gpio.hpp
+++ modules/hal_gpio/src/gpio.hpp
@@ -3,72 +3,76 @@
 
 #include <stdint.h>
 
+namespace hal::gpio
+{
 
-namespace hal::gpio{
+    inline constexpr uint8_t kGPIODirInput = 0x00;
+    inline constexpr uint8_t kGPIODirOutput = 0x01;
 
-inline constexpr uint8_t kGPIODirInput = 0x00;
-inline constexpr uint8_t kGPIODirOutput = 0x01;
+    typedef enum
+    {
+        kGPIOPortA,
+        kGPIOPortB,
+        kGPIOPortC
+    } GPIOPort;
 
-typedef enum{
-    kGPIOPortA,
-    kGPIOPortB,
-    kGPIOPortC
-}GPIOPort;
+    typedef enum
+    {
+        kGPIONormalDriveStrength,
+        kGPIOStrongDriveStrength
+    } GPIODriveStrength;
 
-typedef enum{
-    kGPIONormalDriveStrength,
-    kGPIOStrongDriveStrength
-}GPIODriveStrength;
+    typedef enum
+    {
+        kGPIOFunctionA = 0x0,
+        kGPIOFunctionB = 0x1,
+        kGPIOFunctionC = 0x2,
+        kGPIOFunctionD = 0x3,
+        kGPIOFunctionE = 0x4,
+        kGPIOFunctionF = 0x5,
+        kGPIOFunctionG = 0x6,
+        kGPIOFunctionH = 0x7,
+        kGPIOFunctionI = 0x8,
+        kGPIOFunctionJ = 0x9,
+        kGPIOFunctionK = 0xA,
+        kGPIOFunctionL = 0xB,
+        kGPIOFunctionM = 0xC,
+        kGPIOFunctionN = 0xD
+    } GPIOPinFunction;
 
-typedef enum{
- kGPIOFunctionA,
- kGPIOFunctionB,
- kGPIOFunctionC,
- kGPIOFunctionD,
- kGPIOFunctionE,
- kGPIOFunctionF,
- kGPIOFunctionG
-}GPIOPinFunction;
+    typedef enum
+    {
+        kGPIOPullDown,
+        kGPIOPullUp,
+        kGPIONoPull
+    } GPIOPull;
 
-typedef enum{
-kGPIOPullDown,
-kGPIOPullUp,
-kGPIONoPull
-}GPIOPull;
+    typedef enum
+    {
+        kGPIOSampleOnDemand,
+        kGPIOSampleContinuously
+    } GPIOSamplingMode;
 
-typedef enum{
-kGPIOSampleOnDemand,
-kGPIOSampleContinuously
-}GPIOSamplingMode;
+    typedef enum
+    {
+        kGPIOEventOut,
+        kGPIOEventSet,
+        kGPIOEventClr,
+        kGPIOEventTgl
+    } GPIOInputEvent;
 
-typedef enum{
-kGPIOEventOut,
-kGPIOEventSet,
-kGPIOEventClr,
-kGPIOEventTgl
-}GPIOInputEvent;
+    void SetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num, uint8_t level);
+    void ToggleGPIOPin(GPIOPort port_num, uint8_t pin_num);
+    void SetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num, uint8_t direction);
+    void SetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num, GPIOPinFunction pin_function);
+    void SetGPIOPinDriveStrength(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength drive_strength);
+    void SetGPIOPinSamplingMode(GPIOPort port_num, uint8_t pin_num, GPIOSamplingMode sampling_mode);
 
-void SetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num, uint8_t level);
-void ToggleGPIOPin(GPIOPort port_num, uint8_t pin_num);
-void SetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num, uint8_t direction);
-void SetGPIOPinDriveSetting(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength driver_strength);
-void SetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num, GPIOPinFunction pin_function);
-void SetGPIOPinDriveStrength(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength drive_strength);
-void SetGPIOPinSamplingMode(GPIOPort port_num, uint8_t pin_num, GPIOSamplingMode sampling_mode);
-
-
-uint8_t GetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num);
-uint8_t GetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num);
-uint8_t GetGPIODriveStrength(GPIOPort port_num, uint8_t pin_num);
-uint8_t GetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num);
+    uint8_t GetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num);
+    uint8_t GetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num);
+    uint8_t GetGPIODriveStrength(GPIOPort port_num, uint8_t pin_num);
+    uint8_t GetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num);
 
 }
-
-
-
-
-
-
-
 
 #endif
