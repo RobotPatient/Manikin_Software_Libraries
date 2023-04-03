0454234b833106f468fabc9e1ca267f32c17145b
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-04-03 11:27:08 +0200

    -enh fixed github workflow errors

--- modules/hal_gpio/src/gpio.hpp
+++ modules/hal_gpio/src/gpio.hpp
@@ -1,29 +1,52 @@
+/* *******************************************************************************************
+ * Copyright (c) 2023 by RobotPatient Simulators
+ *
+ * Authors: Thomas van den Oever en Victor Hogeweij
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ *
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so,
+ *
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+***********************************************************************************************/
 #ifndef GPIO_HPP
 #define GPIO_HPP
 
 #include <stdint.h>
 
-namespace hal::gpio
-{
+namespace hal::gpio {
 
     inline constexpr uint8_t kGPIODirInput = 0x00;
     inline constexpr uint8_t kGPIODirOutput = 0x01;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIOPortA,
         kGPIOPortB,
         kGPIOPortC
     } GPIOPort;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIONormalDriveStrength,
         kGPIOStrongDriveStrength
     } GPIODriveStrength;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIOFunctionA = 0x0,
         kGPIOFunctionB = 0x1,
         kGPIOFunctionC = 0x2,
@@ -40,21 +63,18 @@ namespace hal::gpio
         kGPIOFunctionN = 0xD
     } GPIOPinFunction;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIOPullDown,
         kGPIOPullUp,
         kGPIONoPull
     } GPIOPull;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIOSampleOnDemand,
         kGPIOSampleContinuously
     } GPIOSamplingMode;
 
-    typedef enum
-    {
+    typedef enum {
         kGPIOEventOut,
         kGPIOEventSet,
         kGPIOEventClr,
@@ -73,6 +93,6 @@ namespace hal::gpio
     uint8_t GetGPIODriveStrength(GPIOPort port_num, uint8_t pin_num);
     uint8_t GetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num);
 
-}
+}  // namespace hal::gpio
 
 #endif
