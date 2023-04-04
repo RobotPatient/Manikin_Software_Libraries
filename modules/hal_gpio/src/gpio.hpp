c0380754f6d37dcf042ee1af157705342fb81f28
Victor Hogeweij <hogeweyv@gmail.com>
2023-04-04 10:37:28 +0200

    -add Doxygen description for functions

--- modules/hal_gpio/src/gpio.hpp
+++ modules/hal_gpio/src/gpio.hpp
@@ -80,17 +80,88 @@ namespace hal::gpio {
         kGPIOEventClr,
         kGPIOEventTgl
     } GPIOInputEvent;
-
+    /**
+     * @brief Set the (output) level of the pin given.
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @param level This value is either 0 or 1. Where 0 means off, 1 means on (if pin is not inverted :) )..
+     */
     void SetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num, uint8_t level);
+    /**
+     * @brief Toggle (output) level of the given pin.
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     */
     void ToggleGPIOPin(GPIOPort port_num, uint8_t pin_num);
+    /**
+     * @brief Set the pin direction (input or output) of the given pin.
+     * 
+     * @param port_num The port the pin is located on; Example:  PB17 -> PortB, use kGPIOPortB..
+     * @param pin_num The pin number; Example: PB17 -> pin 17;
+     * @param direction The direction of pin (input or output); Use the kGPIODirInput or kGPIODirOutput macro for this.
+     */
     void SetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num, uint8_t direction);
+    /**
+     * @brief Set the pin function of the given pin. (See datasheet pin mux section...)
+     *        For example; When using SERCOM (most of the time) on the pin, the function is either c or d.
+     *        Tip! look in datasheet of the chip for exact mapping data.
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @param pin_function The pin function(group) to assign to the pin. (See datasheet and the GPIOPinFunction enum above!)
+     */
     void SetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num, GPIOPinFunction pin_function);
+    /**
+     * @brief Set the pin drive strength of the given pin. 
+     *        2 mA for normal mode, 8 mA for strong mode at 3.3V supply voltage (see datasheet)
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @param drive_strength The drivestrength to be assigned to the pin. Use enum GPIODriveStrength for this.
+     */
     void SetGPIOPinDriveStrength(GPIOPort port_num, uint8_t pin_num, GPIODriveStrength drive_strength);
+    /**
+     * @brief Set the sampling mode for the given pin.
+     *        Input pins can be sampled either on demand or continuously
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @param sampling_mode The sampling mode to apply to the pin, use enum GPIOSamplingMode for this.
+     */
     void SetGPIOPinSamplingMode(GPIOPort port_num, uint8_t pin_num, GPIOSamplingMode sampling_mode);
-
+    /**
+     * @brief Get the current pin level of the given (input) pin.
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @return uint8_t The current pin level of the input pin.
+     */
     uint8_t GetGPIOPinLevel(GPIOPort port_num, uint8_t pin_num);
+    /**
+     * @brief Get the currently set pin direction (input or output)
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @return uint8_t The current pin direction of the given pin.
+     */
     uint8_t GetGPIOPinDirection(GPIOPort port_num, uint8_t pin_num);
+    /**
+     * @brief Get the currently set drivestrength of output pin. 
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @return uint8_t The current drive strength of the given pin.
+     */
     uint8_t GetGPIODriveStrength(GPIOPort port_num, uint8_t pin_num);
+    /**
+     * @brief Get the currently set pin function for the given pin.
+     * 
+     * @param port_num The port the pin is located on; Example:  PA17 -> PortA, use kGPIOPortA..
+     * @param pin_num The pin number; Example: PA17 -> pin 17;
+     * @return uint8_t The pin function of the given pin.
+     */
     uint8_t GetGPIOPinFunction(GPIOPort port_num, uint8_t pin_num);
 
 }  // namespace hal::gpio
