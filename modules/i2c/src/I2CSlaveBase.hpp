b19db90fb7546c976b29449afe5a3bf4c1e97026
Thomas <TCJ.vandenOever@student.han.nl>
2023-04-17 16:06:59 +0200

    -wip Still working on I2C

---
+++ modules/i2c/src/I2CSlaveBase.hpp
@@ -0,0 +1,47 @@
+/* *******************************************************************************************
+ * Copyright (c) 2023 by RobotPatient Simulators
+ *
+ * Authors: Thomas van den Oever
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ *
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so,
+ *
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ ***********************************************************************************************/
+
+#ifndef I2CSLAVEBASE_HPP
+#define I2CSLAVEBASE_HPP
+
+#include "I2CBase.hpp"
+
+namespace hal::i2c
+{
+    class I2CSlaveBase : protected I2CBase
+    {
+    public:
+        I2CSlaveBase(I2C_PERIPHERAL_T i2c_peripheral, I2CSpeed speed, I2CAddr i2c_addr) : I2CBase(i2c_peripheral, speed, i2c_addr)
+        {
+        }
+
+    private:
+    };
+}
+
+#endif
