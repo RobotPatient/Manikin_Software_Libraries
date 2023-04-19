cc48a1d7ba52f2e043a328b35b7c55cd42059590
Thomas <TCJ.vandenOever@student.han.nl>
2023-04-19 11:52:50 +0200

    -wip I2C -enh classdiagram

--- modules/hal_i2c/src/I2CBase.hpp
+++ modules/hal_i2c/src/I2CBase.hpp
@@ -41,6 +41,20 @@
 
 namespace hal::i2c
 {
+  /// @brief error codes for i2c, should be extention of existing error handler enum
+  enum ErrorCode : uint8_t
+  {
+    SUCCESS,
+    ZERO_BYTES_WRITTEN,
+    NOT_ENOUGH_BUFFER_SPACE,
+    EOT_DATA_TOO_LONG,
+    EOT_NACK_ADDRESS,
+    EOT_NACK_DATA,
+    EOT_OTHER_ERROR,
+    EOT_ERROR_UNKNOWN
+
+  };
+
   /// @brief Possible speed to use for the I2C protocol
   typedef enum
   {
@@ -53,7 +67,7 @@ namespace hal::i2c
   {
   public:
     /// @brief
-    /// @param i2c_peripheral
+    /// @param i2c_peripheral TwoWire * to the
     /// @param speed
     /// @param i2c_addr
     I2CBase(I2C_PERIPHERAL_T i2c_peripheral, I2CSpeed speed, I2CAddr i2c_addr) : i2c_peripheral_(i2c_peripheral_), i2c_addr_(i2c_addr), speed_(speed) {}
@@ -63,19 +77,29 @@ namespace hal::i2c
     /// @param speed
     I2CBase(I2C_PERIPHERAL_T i2c_peripheral, I2CSpeed speed) : i2c_peripheral_(i2c_peripheral), speed_(speed) {}
 
-    virtual void ReadBytes(uint8_t *buffer, uint8_t num_of_bytes);
-    virtual void SendBytes(uint8_t *buffer, uint8_t num_of_bytes);
+    void Init();
+    virtual ErrorCode ReadBytes(uint8_t *buffer, uint8_t num_of_bytes);
+    virtual ErrorCode SendBytes(uint8_t *buffer, uint8_t num_of_bytes);
     virtual void ChangeAddress(I2CAddr new_i2c_address);
 
   protected:
-    void Init();
     void WriteReg(uint16_t reg, uint8_t data);
     void WriteReg16(uint16_t reg, uint16_t data);
     uint8_t ReadReg(uint16_t reg);
     uint16_t ReadReg16(uint16_t reg);
 
+    /// @brief Sends error message to error/warning/log ringbuffer so it can later be send to the mainboard and be stored there for later access
+    /// @param errorIndex index number of error in the errorTableEndTransmission
+    // TODO: refactor this into error handler class/lib
+    void HandleError(uint8_t error);
+
+    /// @brief helper function to handle end of transmisstion errors
+    /// @param code
+    /// @return ErrorCode error
+    ErrorCode HandleEndTransmisstionError(uint8_t code);
+
   private:
-    I2CAddr i2c_addr_;
+    I2CAddr i2c_addr_ = NO_ADDR;
     I2C_PERIPHERAL_T i2c_peripheral_;
     I2CSpeed speed_;
   };
