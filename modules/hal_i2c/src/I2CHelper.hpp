6a246040cea45aeb477dc20799bd8836a8df66af
Thomas <TCJ.vandenOever@student.han.nl>
2023-04-20 18:32:42 +0200

    -enh I finally figured out how the auto formatter works

--- modules/hal_i2c/src/I2CHelper.hpp
+++ modules/hal_i2c/src/I2CHelper.hpp
@@ -7,9 +7,9 @@
  * of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction,
  *
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
- * is furnished to do so,
+ * including without limitation the rights to use, copy, modify, merge, publish,
+ *distribute, sublicense, and/or sell copies of the Software, and to permit
+ *persons to whom the Software is furnished to do so,
  *
  * subject to the following conditions:
  *
@@ -21,9 +21,9 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
  *
  * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ *OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ *OR OTHER DEALINGS IN THE SOFTWARE.
  ***********************************************************************************************/
 
 #ifndef I2CHELPER_HPP
@@ -33,36 +33,33 @@
 
 #ifdef __arm__
 #include <Wire.h>
-#define I2C_PERIPHERAL_T TwoWire *
+#define I2C_PERIPHERAL_T TwoWire*
 #else
 #include "i2c_peripheral_mock.hpp"
-#define I2C_PERIPHERAL_T I2CPeripheralMock *
+#define I2C_PERIPHERAL_T I2CPeripheralMock*
 #endif
 
 namespace hal::i2c {
 /// @brief List of addresses for the I2C slaves. Add more if needed
-enum I2CAddr: uint8_t {
-    NO_ADDR = 0,
-    BREATHING_MODULE = 1,
-    SENSOR_HUB = 2
-};
+enum I2CAddr : uint8_t { NO_ADDR = 0, BREATHING_MODULE = 1, SENSOR_HUB = 2 };
 
-/// @brief error codes for i2c, should be extention of existing error handler enum
+/// @brief error codes for i2c, should be extention of existing error handler
+/// enum
 enum ErrorCode {
-    SUCCESS,
-    ZERO_BYTES_WRITTEN,
-    NOT_ENOUGH_BUFFER_SPACE,
-    EOT_DATA_TOO_LONG,
-    EOT_NACK_ADDRESS,
-    EOT_NACK_DATA,
-    EOT_OTHER_ERROR,
-    EOT_ERROR_UNKNOWN
+  SUCCESS,
+  ZERO_BYTES_WRITTEN,
+  NOT_ENOUGH_BUFFER_SPACE,
+  EOT_DATA_TOO_LONG,
+  EOT_NACK_ADDRESS,
+  EOT_NACK_DATA,
+  EOT_OTHER_ERROR,
+  EOT_ERROR_UNKNOWN
 };
 
 /// @brief Possible speed to use for the I2C protocol
 typedef enum {
-    kI2cSpeed_100KHz = 100000,
-    kI2cSpeed_400KHz = 400000,
+  kI2cSpeed_100KHz = 100000,
+  kI2cSpeed_400KHz = 400000,
 } I2CSpeed;
 }  // namespace hal::i2c
 
